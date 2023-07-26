21bdd8fee7a0109a28b57deb8167116caa059963
tomas-sucena <tomas21sucena@gmail.com>
2023-07-26 23:52:35 +0100

    Refactor main()
    Decrease time complexity by replacing string comparison with strcmp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
+#include <cstring>
 #include <stdexcept>
 
 #include "manager/manager.h"
 
 int main(int argc, char *argv[]) {
     if (argc < 2) return 0;
-    std::string command(argv[1]);
 
-    if (command == "init")
+    if (!strcmp(argv[1], "init"))
         Helpy::Manager::init((argc < 3) ? "" : argv[2], (argc < 4) ? "helpy" : argv[3]);
-    else if (command == "run")
+    else if (!strcmp(argv[1], "init"))
         Helpy::Manager::run((argc < 3) ? "" : argv[2], (argc < 4) ? "Helpyfile" : argv[3]);
-    else
+    else {
+        std::string command(argv[1]);
         throw std::runtime_error("Error: Undefined command '" + command + "'!");
+    }
 
     return 0;
 }
