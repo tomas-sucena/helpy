3ea40f3b4d8f412e0d3d229da1b3a5bf789c4c0d
Sucena <tomas21sucena@gmail.com>
2023-08-05 23:46:13 +0100

    Fix manager
    Fix code that writes the Helpyfile template

--- src/manager/manager.cpp
+++ src/manager/manager.cpp
@@ -7,8 +7,6 @@
 #include "../parser/parser.h"
 #include "../writer/writer.h"
 
-#define LINE_BREAK std::endl << std::endl
-
 namespace Helpy {
     void Manager::formatPath(std::string &path) {
         if (!path.empty() && path.back() != '/')
@@ -22,29 +20,31 @@ namespace Helpy {
         file << "/*\n"
              << "This will be the name of your class. It will also be used to name the generated code files.\n"
              << "Only letters, digits and '_' are allowed. The name must NOT start with a number.\n"
-             << "\n"
+             << '\n'
              << "Note: If this field is omitted, the class will be named Helpy.\n"
-             << "*/\n";
+             << "*/\n"
+             << "NAME: Helpy\n";
 
-        file << "NAME: Helpy" << LINE_BREAK;
-
-        // arguments
-        file << "/*\n"
-             << "This specifies the number of arguments each Helpy command will have.\n"
-             << "*/\n";
-
-        file << "ARGUMENTS: 3" << LINE_BREAK;
+        // color
+        file << '\n'
+             << "/*\n"
+             << "This will be the main color used to style the command line.\n"
+             << "It can be one of the following: red, green, yellow, blue, purple, cyan or white.\n"
+             << '\n'
+             << "Note: If this field is omitted, the selected color will be yellow.\n"
+             << "*/\n"
+             << "COLOR: yellow\n";
 
         // commands
-        file << "/*\n"
+        file << '\n'
+             << "/*\n"
              << "The following lines detail the Helpy commands.\n"
              << "To add a command, simply enter a new line, type '-' and write the keywords\n"
              << "(eg: - RUN SORTING ALGORITHM).\n"
              << "\n"
              << "Note: Commands are case insensitive.\n"
-             << "*/\n";
-
-        file << "COMMANDS:\n"
+             << "*/\n"
+             << "COMMANDS:\n"
              << "- // write your command here\n";
     }
 
